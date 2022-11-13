37a679a84c0bfde15ceeb380d346b4272db912af
Adithya Bandara <104627196+AdithyaBandara@users.noreply.github.com>
2022-11-13 08:33:25 +0530

    databasecreated

---
+++ docs/javascript/firebase.js
@@ -0,0 +1,97 @@
+    // Import the functions you need from the SDKs you need
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+    import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+    import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+    // Your web app's Firebase configuration
+
+    const firebaseConfig = {
+
+      apiKey: "AIzaSyDA_0VCQuWj9K-IrQXYCSDRph1WIZQlHoc",
+
+      authDomain: "agoralms.firebaseapp.com",
+
+      projectId: "agoralms",
+
+      storageBucket: "agoralms.appspot.com",
+
+      messagingSenderId: "411003821626",
+
+      appId: "1:411003821626:web:5206520927eee85717df03"
+
+    };
+
+
+    // Initialize Firebase
+
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase(app);
+    const auth = getAuth();
+
+
+    const signup = document.getElementById('signup');
+    signup.addEventListener('click', (e) => {
+      e.preventDefault();
+      const username = document.getElementById('username').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          // ...
+          set(ref(db, 'users/' + user.uid), {
+            username: username,
+            email: email,
+          });
+          alert("User created successfully");
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          alert(errorMessage);
+          // ..
+        });
+    })
+
+    const login = document.getElementById('login');
+    login.addEventListener('click', (e) => {
+      e.preventDefault();
+      const email = document.getElementById('un').value;
+      const password = document.getElementById('pw').value;
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+          const dt = new Date();
+          update(ref(db, 'users/' + user.uid), {
+            lastLogin: dt,
+          });
+          alert("User logged in successfully");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          alert(errorMessage);
+        });
+    })
+
+    const user = auth.currentUser;
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
